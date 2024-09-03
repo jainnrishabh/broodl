e46445085cd2b267c87f9b854e682d230b879ab3
jainnrishabh <jainnrishabh1234@gmail.com>
2024-09-03 01:30:01 -0400

    issue related to metadata resolved

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import Main from "@/components/Main";
 import Dashboard from "@/components/Dashboard";
 
-export const metadata: Metadata = {
+export const metadata = {
     title: "Broodl ⋅ Dashboard",
     description: "Track your daily mood every day of the year",
   };
