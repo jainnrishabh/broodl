550eef243891d40632c1b4a80df432c672dbd1bb
jainnrishabh <jainnrishabh1234@gmail.com>
2024-09-05 17:40:22 -0400

    resume url attached

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,9 +30,9 @@ export default function RootLayout({
 
   const footer = (
     <footer className="p-4 sm:p-8 grid place-items-center">
-      
+      <Link href={'https://jainnrishabh.vercel.app'} target="_blank" className="">
         <p className={'text-indigo-500 duration-200 hover:text-white hover:bg-indigo-500  ' + fugaz.className}>Built by Rishabh Jain 💛</p>
-      
+      </Link>
     </footer>
   )
 
